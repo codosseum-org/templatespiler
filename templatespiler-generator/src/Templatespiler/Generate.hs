0ac766d511d8b14acb83acbf9713a8f1fcf5a590
Alexander Wood <alexljwood24@hotmail.co.uk>
2023-08-28 19:31:19 +0100

    make generator outputs a little more sane

--- templatespiler-generator/src/Templatespiler/Generate.hs
+++ templatespiler-generator/src/Templatespiler/Generate.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE LexicalNegation #-}
+
 module Templatespiler.Generate where
 
 import Data.Text qualified as Text
@@ -7,6 +9,12 @@ import Hedgehog.Range qualified as Range
 
 import Language.Templatespiler.Syntax
 import Prelude hiding (Type)
+
+arbitrarySaneInt :: Gen Int
+arbitrarySaneInt = Gen.int (Range.linear -1000 1000)
+
+arbitrarySaneFloat :: Gen Double
+arbitrarySaneFloat = Gen.double (Range.linearFrac (-1000) 1000)
 
 arbitraryInput :: BindingList -> Gen [Text]
 arbitraryInput (BindingList bs) = join <$> traverse arbitraryBinding (toList bs)
@@ -41,6 +49,6 @@ arbitraryType _ (TerminalType t) = arbitraryTerminalType t
 arbitraryType mSep (CombinatorType c) = arbitraryCombinatorWithSep mSep c
 
 arbitraryTerminalType :: TerminalType -> Gen [Text]
-arbitraryTerminalType IntType = pure . show <$> Gen.int Range.linearBounded
+arbitraryTerminalType IntType = pure . show <$> arbitrarySaneInt
 arbitraryTerminalType StringType = pure . toText <$> Gen.string (Range.linear 1 20) Gen.alphaNum
-arbitraryTerminalType FloatType = pure . show <$> Gen.double (fromIntegral <$> Range.linearBounded @Int)
+arbitraryTerminalType FloatType = pure . show <$> arbitrarySaneFloat

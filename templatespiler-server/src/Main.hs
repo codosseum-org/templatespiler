0802f27393514872872d2a98e7f9d7247c64db2c
Alexander Wood <alexljwood24@hotmail.co.uk>
2023-08-29 19:20:57 +0100

    separate apps and libraries for all modules

--- templatespiler-server/src/Main.hs
+++ templatespiler-server/src/Main.hs
@@ -1,20 +1,5 @@
 module Main where
 
-import Hedgehog.Gen (sample)
-import Language.Templatespiler.Parser
-import Main.Utf8 qualified as Utf8
-import Templatespiler.Generate (arbitraryInput)
-import Text.Trifecta (parseFromFile)
-
 main :: IO ()
 main = do
-  Utf8.withUtf8 $ do
-    tokens <- parseFromFile parseBindingList "./test.tmpspl"
-    case tokens of
-      Nothing -> exitFailure
-      Just bs -> do
-        putStrLn ""
-        i <- sample $ arbitraryInput bs
-        traverse_ putTextLn i
-
-    putStrLn ""
+  putStrLn ""

523d6552a99b836598015eba343664e36b62fcec
Alexander Wood <alexljwood24@hotmail.co.uk>
2024-06-11 19:30:19 +0100

    add some documentation for types

--- templatespiler-parser/src/Language/Templatespiler/Syntax.hs
+++ templatespiler-parser/src/Language/Templatespiler/Syntax.hs
@@ -11,14 +11,30 @@ newtype Ident = Ident Text deriving (Show, Eq)
 
 data Binding = Binding Ident Type deriving (Show, Eq)
 
-data Type = TerminalType TerminalType | CombinatorType Combinator deriving (Show, Eq)
+data Type
+  = TerminalType TerminalType
+  | CombinatorType Combinator
+  deriving (Show, Eq)
 
-data TerminalType = IntType | FloatType | StringType deriving (Show, Eq)
+-- | The terminal / atomic types that can be used in a binding.
+data TerminalType
+  = -- | An integer.
+    IntType
+  | -- | A floating point number.
+    FloatType
+  | -- | A string.
+    StringType
+  deriving (Show, Eq)
 
 data Combinator
-  = NamedCombinator Ident Type
-  | GroupCombinator BindingList
-  | ArrayCombinator Int Type
-  | SepByCombinator Text BindingList
-  | ListCombinator Type
+  = -- | A named combinator, e.g. @foo: int@.
+    NamedCombinator Ident Type
+  | -- | A group of bindings, eg @[x : Integer, y : Integer]@
+    GroupCombinator BindingList
+  | -- | An array of a given length, eg @array 3 (num : Integer)@
+    ArrayCombinator Int Type
+  | -- | A list of bindings separated by a given string, eg @sep-by " " [x : Integer, y : Integer]@
+    SepByCombinator Text BindingList
+  | -- | A list (dynamic array) of a given type, eg @list (num : Integer)@
+    ListCombinator Type
   deriving (Show, Eq)
